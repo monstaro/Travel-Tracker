6fc24bfde5afa5c9c74ed50de7988167d92249f6
monstaro <monstaro32@gmail.com>
2020-02-26 20:26:24 -0700

    Begins testing

---
+++ test/agent-test.js
@@ -0,0 +1,46 @@
+const expect = chai.expect;
+
+import chai from 'chai';
+
+import Traveler from '../src/classes/traveler.js';
+
+import Agent from '../src/classes/agent.js';
+
+import travelerData from '../data/sample-travelers.js';
+
+import tripData from '../data/sample-trips.js';
+
+import destinationData from '../data/sample-destinations.js';
+
+
+let agent;
+let destination;
+
+describe('Agent', function() {
+  beforeEach(() => {
+    agent = new Agent()
+    destination = destinationData.destinations
+  })
+  it('should be a function', () => {
+    expect(Agent).to.be.a('function');
+  });
+  it('should instantiate a new agent', () => {
+    expect(agent).to.be.an.instanceof(Agent)
+  })
+  it('should see all pending requests', () => {
+    agent.findPendingRequests(tripData)
+    expect(agent.pendingRequests.length).to.equal(2)
+  })
+  it('should see all approved requests', () => {
+    agent.findApprovedRequests(tripData)
+    expect(agent.approvedRequests.length).to.equal(13)
+  })
+  it('should have a total income', () => {
+    agent.findApprovedRequests(tripData)
+    agent.getTotalIncome(destination)
+    expect(agent.income).to.equal(5819)
+  })
+  it('should be able to determine the number of travelers on todays date', () => {
+    
+  })
+});
---
+++ test/traveler-test.js
@@ -0,0 +1,49 @@
+const expect = chai.expect;
+
+import chai from 'chai';
+
+import Traveler from '../src/classes/traveler.js';
+
+import travelerData from '../data/sample-travelers.js';
+
+import tripData from '../data/sample-trips.js';
+
+let traveler
+
+describe('Traveler', function() {
+  beforeEach(() => {
+    traveler = new Traveler(travelerData.travelers[3])
+  })
+  it('should be a function', () => {
+    expect(Traveler).to.be.a('function');
+  });
+  it('should instantiate a new traveler', () => {
+    expect(traveler).to.be.an.instanceof(Traveler)
+  })
+  it('should have an id', () => {
+    expect(traveler.id).to.equal(4)
+  })
+  it('should have an name', () => {
+    expect(traveler.name).to.equal('Leila Thebeaud')
+  })
+  it('should have a type of travel', () => {
+    expect(traveler.travelerType).to.equal('photographer')
+  })
+  it('should see all of the users trips', () => {
+    expect(traveler.seeAllTrips(tripData)).to.deep.equal([{
+      "id": 1,
+      "userID": 4,
+      "destinationID": 49,
+      "travelers": 1,
+      "date": "2019/09/16",
+      "duration": 8,
+      "status": "approved",
+      "suggestedActivities": [
+      
+      ]
+    }])
+  })
+  it('should be able to make a trip request', () => {
+    
+  })
+});
