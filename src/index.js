48f1e9d5586154c42b379eda6d9bc284fa4324d7
monstaro <monstaro32@gmail.com>
2020-02-27 18:43:39 -0700

    Finish login functionality

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@
 // An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
 import dataController from './dataController.js'
+import domUpdates from './domUpdates.js'
+import allTrips from './classes/allTrips.js'
+import Traveler from './classes/traveler.js'
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
@@ -11,12 +14,52 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-let randomId = Math.floor(Math.random() * 50)
+// let randomId = Math.floor(Math.random() * 50)
 
-console.log(randomId)
+let userName = $('.username-input')
+let password = $('.password-input')
+let agent;
+let traveler;
 
-// traveler50
 
-let userId = $('.user-input').val().substring(8, 9)
-window.on('keypress', console.log(userId))
-$('.submit-button').on('click', dataController.loadSingleUser(userId))
+$('.submit-btn').on('click', function() {
+  event.preventDefault()
+  loginHandler()
+})
+
+const loginHandler = () => {
+  let allUsernames = []
+  for (var i = 0; i < 50; i++) {
+    allUsernames.push(`traveler${i}`)
+  }
+  if (allUsernames.includes(userName.val().toLowerCase()) && password.val() === 'travel2020') {
+    loginTraveler()
+  }
+
+  if (userName.val().toLowerCase() === 'agency' && password.val() === 'travel2020') {
+    loginAgent()
+  } 
+
+}
+
+const loginTraveler = () => {
+  console.log('hey')
+  let id = userName.val().substring(8, 10)
+  domUpdates.loadTraveler()
+  dataController.loadSingleUser(id)
+  // let traveler = new Traveler(user.id, user.name, user.travelType)
+  // console.log(traveler)
+}
+
+const loginAgent = () => {
+  // agent = new Agent()
+  domUpdates.loadAgent()
+}
+
+// const instantiateTrips = (tripData) => {
+//     let trip = new Trip(tripData)
+// }
+
+// loginUser() {
+
+// }
