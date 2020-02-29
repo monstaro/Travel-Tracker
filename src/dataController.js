2c54993ff0b70a6e383a5b850795f1808a6c8361
monstaro <monstaro32@gmail.com>
2020-02-28 19:22:32 -0700

    Instantiates user when called in index.js

--- src/dataController.js
+++ src/dataController.js
@@ -1,20 +1,49 @@
+import Traveler from './classes/traveler.js'
+import domUpdates from './domUpdates.js'
 
-const allUserData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/travelers/travelers')
+const userData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/travelers/travelers')
   .then(response => response.json())
   .then(data => data.travelers)
   .catch(error => console.log(error.message))
 
-const allTripsData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/trips/trips')
+const tripsData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/trips/trips')
   .then(response => response.json())
   .then(data => data.trips)
   .catch(error => console.log(error.message))
 
-const allDestinationsData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/destinations/destinations')
+const destinationsData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/destinations/destinations')
   .then(response => response.json())
   .then(data => data.destinations)
   .catch(error => console.log(error.message))
 
 
+let allUsers;
+let allTrips;
+let allDestinations;
+let traveler;
 
-Promise.all([allUserData, allTripsData, allDestinationsData])
-  .then(data => console.log(data))
+Promise.all([userData, tripsData, destinationsData])
+  .then(data => {
+    allUsers = data[0]
+    allTrips = data[1]
+    allDestinations = data[2]
+  })
+
+
+
+
+const dataController = {
+  loadUser(id) {
+    let user = allUsers[id - 1]
+    let trips = allTrips.filter(trip => trip.userID === parseInt(id))
+
+    traveler = new Traveler(user, trips)
+
+    console.log(traveler)
+    domUpdates.loadTraveler(traveler, allDestinations)
+  }
+}
+
+
+export default dataController;
+
