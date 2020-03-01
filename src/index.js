1f5e932841c132d6e4e6b653f4941fa0512b4aee
monstaro <monstaro32@gmail.com>
2020-03-01 11:26:25 -0700

    Adds dollar$ syntax for income function

--- src/index.js
+++ src/index.js
@@ -1,37 +1,18 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
 import dataController from './dataController.js'
 import domUpdates from './domUpdates.js'
-import allTrips from './classes/allTrips.js'
-import Traveler from './classes/traveler.js'
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
-
-// let randomId = Math.floor(Math.random() * 50)
-
-let userName = $('.username-input')
-let password = $('.password-input')
-
-
-$('.submit-btn').on('click', function() {
-  event.preventDefault()
-  loginHandler()
-})
-
+//LOGIN VALIDATION
 const loginHandler = () => {
+  let userName = $('.username-input');
+  let password = $('.password-input');
   let allUsernames = []
   for (var i = 0; i <= 50; i++) {
     allUsernames.push(`traveler${i}`)
   }
   if (allUsernames.includes(userName.val().toLowerCase()) && password.val() === 'travel2020') {
-    loginTraveler()
+    loginTraveler(userName)
   }
 
   if (userName.val().toLowerCase() === 'agency' && password.val() === 'travel2020') {
@@ -39,6 +20,16 @@ const loginHandler = () => {
   } 
 }
 
+const loginTraveler = (userName) => {
+  let id = userName.val().substring(8, 10)
+  dataController.loadUser(id)
+}
+
+const loginAgent = () => {
+  dataController.loadAgent()
+}
+
+//DISPLAYS TRIPS FROM DROPDOWN ON TRAVELER PAGE
 const travelerClickHandler = (e) => {
   if (e.target.value === "0") {
     domUpdates.displayTrips(null)
@@ -57,26 +48,10 @@ const travelerClickHandler = (e) => {
   }
 }
 
-
-const loginTraveler = () => {
-  let id = userName.val().substring(8, 10)
-  dataController.loadUser(id)
-}
-
-
-const loginAgent = () => {
-  dataController.loadAgent()
-}
-
-
+//EVENT LISTENERS
+$('.submit-btn').on('click', function() {
+  event.preventDefault()
+  loginHandler()
+})
 
 $('select').on('change', travelerClickHandler)
-
-
-// const instantiateTrips = (tripData) => {
-//     let trip = new Trip(tripData)
-// }
-
-// loginUser() {
-
-// }
