6b41c150b73bb33a2830600bb586511e250d18db
monstaro <monstaro32@gmail.com>
2020-02-29 18:23:12 -0700

    Updates agent instantiation

--- src/dataController.js
+++ src/dataController.js
@@ -1,4 +1,5 @@
 import Traveler from './classes/traveler.js'
+import Agent from './classes/agent.js'
 import domUpdates from './domUpdates.js'
 
 const userData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/travelers/travelers')
@@ -21,6 +22,7 @@ let allUsers;
 let allTrips;
 let allDestinations;
 let traveler;
+let agent;
 
 Promise.all([userData, tripsData, destinationsData])
   .then(data => {
@@ -38,12 +40,16 @@ const dataController = {
 
     let trips = allTrips.filter(trip => trip.userID === parseInt(id))
 
-    trips.map(trip => trip.location = allDestinations.filter(destination => destination.id === trip.destinationID))
-
+    
 
     traveler = new Traveler(user, trips)
 
     domUpdates.loadTraveler(traveler, allDestinations)
+  },
+  loadAgent() {
+    allTrips.map(trip => trip.location = allDestinations.filter(destination => destination.id === trip.destinationID))
+    //may end up wanting to instantiate the users with their trips / mapping over the allUsers array to add the trips before I push allUsers into the agent.
+    agent = new Agent(allTrips, allUsers)
   }
 }
 
