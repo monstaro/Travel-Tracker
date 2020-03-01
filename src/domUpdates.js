4ee8d851ddbb07469da16271fba5cfbf79af3442
monstaro <monstaro32@gmail.com>
2020-02-29 17:26:32 -0700

    Changes styling for trip summaries

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -16,23 +16,23 @@ const domUpdates = {
     thisTraveler = traveler;
   },
   displayTrips(tripCategory) { 
+    if (!tripCategory) {
+      $('.selected-trips').empty()
+    }
     $('.selected-trips').empty()
     thisTraveler[tripCategory]().forEach(trip => {
       $('.selected-trips').append(`<section class="travler-trip" 
       style="box-shadow: 0px 0px 5px grey;
-             padding: 20px;
+             display: grid;
+             grid-template-columns: 1fr 1fr;
              margin: 20px;
-             display: flex;
-             flex-direction: row;
-             justify-content: space-between">
+             padding: 20px;">
       <div class="trip-image-and-name"
            style="margin: auto">
-      <img src="${trip.location[0].image}" width="50%" height="auto">
+      <img src="${trip.location[0].image}" width="100%" height="auto">
       <h2>${trip.location[0].destination}</h2>
       </div>
-      <div class="trip-summary"
-           style="display: flex;
-                  flex-direction: column;">
+      <div class="trip-summary">
       <h3> Your trip to ${trip.location[0].destination.split(',')[0]}.</h3>
       <h3>Start date: ${trip.date}</h3>
       <h3>Trip duration: ${trip.duration} days. </h3>
