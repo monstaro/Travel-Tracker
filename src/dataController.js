8bbc1b525f72ef5aa2956da060dbfa0631b24e76
monstaro <monstaro32@gmail.com>
2020-03-02 13:10:05 -0700

    Instantiates new trip and creates post request

--- src/dataController.js
+++ src/dataController.js
@@ -2,6 +2,7 @@
 import Traveler from './classes/traveler.js'
 import Agent from './classes/agent.js'
 import domUpdates from './domUpdates.js'
+import Trip from './classes/trip.js'
 var moment = require('moment');
 
 
@@ -11,6 +12,7 @@ let allTrips;
 let allDestinations;
 let traveler;
 let agent;
+let trip;
 
 //FETCH USER DATA
 const userData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/travelers/travelers')
@@ -35,7 +37,7 @@ Promise.all([userData, tripsData, destinationsData])
     allDestinations = data[2];
   })
 
-//INSTANTIATIONS
+//  INSTANTIATIONS / POSTING
 const dataController = {
   loadUser(id) {
     let user = allUsers[id - 1];
@@ -44,7 +46,10 @@ const dataController = {
     trips.map(trip => trip.location = allDestinations.filter(destination => destination.id === trip.destinationID));
 
     trips.map(trip => {
+
       trip.date.split('-').join('/')
+      // ^^ put in conditional?
+
       if (typeof trip.destinationID === 'string') {
         trip.destinationID = parseInt(trip.destinationID)
       }
@@ -55,7 +60,6 @@ const dataController = {
       }
     }
     )
-    
     traveler = new Traveler(user, trips);
     domUpdates.loadTraveler(traveler, allDestinations);
   },
@@ -64,6 +68,26 @@ const dataController = {
     //may end up wanting to instantiate the users with their trips / mapping over the allUsers array to add the trips before I push allUsers into the agent.
     agent = new Agent(allTrips, allUsers);
     domUpdates.loadAgent(agent, allDestinations);
+  },
+  createNewTripInstance(user, destination, travelerCount, startDate, duration) {
+    trip = new Trip(user, destination, travelerCount, startDate, duration)
+    domUpdates.displayConfirmationPage(trip)
+  },
+  postTrip(trip) {
+    fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/trips/trips', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(trip),
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.log(error.message))
+
+    // {id: <number>, userID: <number>, destinationID: <number>, travelers: <number>, date: <string 'YYYY/MM/DD'>, duration: <number>, status: <string 'approved' or 'pending'>, suggestedActivities: <array of strings>}
+
+
   }
 }
 
