b4e6d7af87915acad3070076a72848000c0d4857
monstaro <monstaro32@gmail.com>
2020-03-03 20:20:22 -0700

    Creates test for cost of trip method

--- test/utilities-test.js
+++ test/utilities-test.js
@@ -35,4 +35,25 @@ describe('Utility', function() {
       "alt": "overview of city buildings with a clear sky"
     })
   })
+  it('should find the price of a trip', () => {
+    expect(utility.findCostOfTrip({
+      "id": 1,
+      "destination": "Lima, Peru",
+      "estimatedLodgingCostPerDay": 70,
+      "estimatedFlightCostPerPerson": 400,
+      "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+      "alt": "overview of city buildings with a clear sky"
+    }, {
+      "id": 1,
+      "userID": 44,
+      "destinationID": 49,
+      "travelers": 1,
+      "date": "2019/09/16",
+      "duration": 8,
+      "status": "approved",
+      "suggestedActivities": [
+        
+      ]
+    })).to.equal('4136')
+  })
 });
